605ff8afdaad414864493e0092d426e5d96e2123
lary <songlairui@qq.com>
2018-10-05 23:10:46 +0800

    init 依赖

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const path = require('path')
+
+const development = {
+  username: 'root',
+  password: null,
+  database: 'user_development',
+  host: '127.0.0.1',
+  dialect: 'sqlite',
+  storage: path.resolve(__dirname, '../db', 'database.sqlite')
+}
+const config = {
+  development: development,
+  test: {
+    ...development,
+    database: 'user_test'
+  },
+  production: {
+    ...development,
+    database: 'user_production'
+  }
+}
+module.exports = config
